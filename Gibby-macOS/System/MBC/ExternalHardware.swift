ab95e486ab3b689da22279b7d78edcad8752025a
Kevin Vitale <kevinvitale@gmail.com>
2019-02-04 11:56:38 -0500

    [MBC] moved 'ExternalHardware' to own file

---
+++ Gibby-macOS/System/MBC/ExternalHardware.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+extension MemoryController {
+    public struct ExternalHardware: OptionSet, CustomStringConvertible {
+        public let rawValue: Int
+        public init(rawValue: Int) {
+            self.rawValue = rawValue
+        }
+        
+        public static let ram       = ExternalHardware(rawValue: 1 << 0)
+        public static let battery   = ExternalHardware(rawValue: 1 << 1)
+        public static let timer     = ExternalHardware(rawValue: 1 << 2)
+        public static let rumble    = ExternalHardware(rawValue: 1 << 3)
+        public static let sensor    = ExternalHardware(rawValue: 1 << 4)
+        public static let camera    = ExternalHardware(rawValue: 1 << 5)
+        
+        public var description: String {
+            switch self.rawValue {
+            case ExternalHardware.ram.rawValue:     return "RAM"
+            case ExternalHardware.battery.rawValue: return "BATTERY"
+            case ExternalHardware.timer.rawValue:   return "TIMER"
+            case ExternalHardware.rumble.rawValue:  return "RUMBLE"
+            case ExternalHardware.sensor.rawValue:  return "SENSOR"
+            case ExternalHardware.camera.rawValue:  return "CAMERA"
+            default: return ""
+            }
+        }
+    }
+}
