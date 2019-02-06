30410ed4b01ed24c529a3e52d9dbb6691efb5bc8
Kevin Vitale <kevinvitale@gmail.com>
2019-02-06 12:28:08 -0500

    [MBC] basic 'MBC' struct. Needs work.

--- Gibby-macOS/System/MBC/MemoryController.swift
+++ Gibby-macOS/System/MBC/MemoryController.swift
@@ -1,2 +1,11 @@
 public struct MemoryController {
+    public init(configuration: Configuration = .rom(ram: false, battery: false), romSize: Any? = nil, ramSize: Any? = nil) {
+        self.configuration = configuration
+        self.romSize = romSize
+        self.ramSize = ramSize
+    }
+    
+    public let configuration: Configuration
+    public let romSize: Any?
+    public let ramSize: Any?
 }
