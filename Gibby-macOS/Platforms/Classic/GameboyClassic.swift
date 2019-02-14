888888d8b5b7a7b2601cbfa57f5d9dd0bcbc2ddd
Kevin Vitale <kevinvitale@gmail.com>
2019-02-14 14:45:24 -0500

    [GBClassic] moves 'Region' into its own file

--- Gibby-macOS/Platforms/Classic/GameboyClassic.swift
+++ Gibby-macOS/Platforms/Classic/GameboyClassic.swift
@@ -13,27 +13,3 @@ public final class GameboyClassic: Platform {
     public static var headerOffset: UInt16 { return 0x100 }
     public static var headerSize: UInt16   { return 0x50  }
 }
-
-extension GameboyClassic {
-    public enum Region: UInt8, ExpressibleByStringLiteral, CustomStringConvertible {
-        case japanese    = 0x00
-        case nonJapanese = 0x01
-        
-        public init(stringLiteral value: String) {
-            if value.capitalized == "Japanese" {
-                self = .japanese
-            }
-            else {
-                self = .nonJapanese
-            }
-        }
-        
-        public var description: String {
-            switch self {
-            case .japanese: return "Japanese"
-            case .nonJapanese: return "Non-Japanese"
-            }
-        }
-    }
-}
-
