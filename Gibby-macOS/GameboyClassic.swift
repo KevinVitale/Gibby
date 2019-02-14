5ce56e8c034f1e017289e461f66bce355d4e2267
Kevin Vitale <kevinvitale@gmail.com>
2019-02-13 20:50:19 -0500

    [GBClassic] adds 'ColorMode' enum

--- Gibby-macOS/GameboyClassic.swift
+++ Gibby-macOS/GameboyClassic.swift
@@ -11,3 +11,32 @@ final class GameboyClassic: Platform {
     typealias AddressSpace  = UInt16
 }
 
+extension GameboyClassic {
+    enum ColorMode: RawRepresentable, Codable {
+        init(rawValue: UInt8) {
+            switch rawValue {
+            case 0x80:
+                self = .some
+            case 0xC0:
+                self = .exclusive
+            default:
+                self = .unknown(rawValue)
+            }
+        }
+        
+        case unknown(UInt8)
+        case some
+        case exclusive
+        
+        var rawValue: UInt8 {
+            switch self {
+            case .some:
+                return 0x80
+            case .exclusive:
+                return 0xC0
+            case .unknown(let value):
+                return value
+            }
+        }
+    }
+}
