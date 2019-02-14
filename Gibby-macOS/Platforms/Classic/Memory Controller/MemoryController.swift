7d4b2bb293fd283d9ec57bd6df379615dac28d9f
Kevin Vitale <kevinvitale@gmail.com>
2019-02-14 09:29:03 -0500

    [MBC] removes unused code (init, props, etc.)

--- Gibby-macOS/Platforms/Classic/Memory Controller/MemoryController.swift
+++ Gibby-macOS/Platforms/Classic/Memory Controller/MemoryController.swift
@@ -1,11 +1,2 @@
 public struct MemoryController {
-    public init(configuration: Configuration = .rom(ram: false, battery: false), romSize: Any? = nil, ramSize: Any? = nil) {
-        self.configuration = configuration
-        self.romSize = romSize
-        self.ramSize = ramSize
-    }
-    
-    public let configuration: Configuration
-    public let romSize: Any?
-    public let ramSize: Any?
 }
