202979c923fc7aff515f6bb64e1b6c63136d781e
Kevin Vitale <kevinvitale@gmail.com>
2019-02-13 22:37:06 -0500

    [GBClassic] class is now 'public'

--- Gibby-macOS/GameboyClassic.swift
+++ Gibby-macOS/GameboyClassic.swift
@@ -1,19 +1,19 @@
 import Foundation
 
-final class GameboyClassic: Platform {
-    static var logo: Data = Data(bytes: [
+public final class GameboyClassic: Platform {
+    public static var logo: Data = Data(bytes: [
         0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B, 0x03, 0x73, 0x00, 0x83, 0x00, 0x0C, 0x00, 0x0D,
         0x00, 0x08, 0x11, 0x1F, 0x88, 0x89, 0x00, 0x0E, 0xDC, 0xCC, 0x6E, 0xE6, 0xDD, 0xDD, 0xD9, 0x99,
         0xBB, 0xBB, 0x67, 0x63, 0x6E, 0x0E, 0xEC, 0xCC, 0xDD, 0xDC, 0x99, 0x9F, 0xBB, 0xB9, 0x33, 0x3E
         ])
     
-    typealias Cartridge     = Any
-    typealias AddressSpace  = UInt16
+    public typealias Cartridge     = Any
+    public typealias AddressSpace  = UInt16
 }
 
 extension GameboyClassic {
-    enum ColorMode: RawRepresentable, Codable {
-        init(rawValue: UInt8) {
+    public enum ColorMode: RawRepresentable, Codable {
+        public init(rawValue: UInt8) {
             switch rawValue {
             case 0x80:
                 self = .some
@@ -28,7 +28,7 @@ extension GameboyClassic {
         case some
         case exclusive
         
-        var rawValue: UInt8 {
+        public var rawValue: UInt8 {
             switch self {
             case .some:
                 return 0x80
@@ -46,7 +46,7 @@ extension GameboyClassic {
         case japanese    = 0x00
         case nonJapanese = 0x01
         
-        init(stringLiteral value: String) {
+        public init(stringLiteral value: String) {
             if value.capitalized == "Japanese" {
                 self = .japanese
             }
@@ -55,7 +55,7 @@ extension GameboyClassic {
             }
         }
         
-        var description: String {
+        public var description: String {
             switch self {
             case .japanese: return "Japanese"
             case .nonJapanese: return "Non-Japanese"
@@ -130,7 +130,7 @@ extension GameboyClassic {
             }
         }
         
-        public var colorMode: ColorMode {
+        public var colorMode: GameboyClassic.ColorMode {
             get {
                 return ColorMode(rawValue: bytes[0x43])
             }
