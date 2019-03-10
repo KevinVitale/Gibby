2aa2ccce80aa1fb8e64fdb685087dd5ddf9e0996
Kevin Vitale <kevinvitale@gmail.com>
2019-03-10 13:46:12 -0400

    [Cartridge] checks to see if ‘Cartridge’ is empty
    Sanity check.

--- Source/Platforms/Protocols/Cartridge.swift
+++ Source/Platforms/Protocols/Cartridge.swift
@@ -12,6 +12,15 @@ extension Cartridge {
     public var header: Header {
         let lowerBound = Self.Index(Platform.headerRange.lowerBound)
         let upperBound = Self.Index(Platform.headerRange.upperBound)
-        return Header(bytes: Data(self[lowerBound..<upperBound]))
+        
+        let headerRange = lowerBound..<upperBound
+        var headerData  = Data(count: headerRange.count)
+
+        guard self.isEmpty == false else {
+            return Header(bytes: headerData)
+        }
+        
+        headerData = Data(self[headerRange])
+        return Header(bytes: headerData)
     }
 }
