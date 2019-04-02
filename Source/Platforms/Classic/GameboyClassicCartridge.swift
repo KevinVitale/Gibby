eb27a4b596a4adf7dc310c2aa8719e4667759640
Kevin Vitale <kevinvitale@gmail.com>
2019-04-02 08:47:48 -0400

    [Cartridge] moved ‘Region’ to its own source file

--- Source/Platforms/Classic/GameboyClassicCartridge.swift
+++ Source/Platforms/Classic/GameboyClassicCartridge.swift
@@ -40,25 +40,3 @@ extension GameboyClassic {
     }
 }
 
-extension GameboyClassic.Cartridge {
-    public enum Region: UInt8, ExpressibleByStringLiteral, CustomStringConvertible {
-        case japanese    = 0x00
-        case nonJapanese = 0x01
-        
-        public init(stringLiteral value: String) {
-            if value.capitalized == "Japanese" {
-                self = .japanese
-            }
-            else {
-                self = .nonJapanese
-            }
-        }
-        
-        public var description: String {
-            switch self {
-            case .japanese: return "Japanese"
-            case .nonJapanese: return "Non-Japanese"
-            }
-        }
-    }
-}
