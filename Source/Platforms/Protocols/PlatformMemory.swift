2ee15d09b08aa910964d82b814c5186cba0d7863
Kevin Vitale <kevinvitale@gmail.com>
2019-04-02 08:48:02 -0400

    [Platform] explicit @import declaration

--- Source/Platforms/Protocols/PlatformMemory.swift
+++ Source/Platforms/Protocols/PlatformMemory.swift
@@ -1,4 +1,4 @@
-import Foundation
+import Foundation.NSData
 
 public protocol PlatformMemory: Collection where Self.Element == Data.Element, Self.Index: FixedWidthInteger {
     associatedtype Platform: Gibby.Platform
